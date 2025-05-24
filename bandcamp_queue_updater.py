cb5ec3d487ccb0ac3ac2b07a279f7c579aee3fe3
Max Herrera <mh1101110@gmail.com>
2025-05-24 14:22:19 -0500

    Add comments to code

--- bandcamp_queue_updater.py
+++ bandcamp_queue_updater.py
@@ -7,13 +7,19 @@ def update_queue(fn):
         data = json.load(json_file)
     
     for song in data["queue"]["p"]:
+        # Missing field
         song["is_visible_in_public_playlist"] = True
+
+        # This used to just be "PLAYLIST"
         song["track_type"] = "OWNED_PLAYLIST"
+
+        # This field was removed
         del song["playlist_id"]
 
     with open(fn, "w", encoding="utf-8") as json_file:
+        # Add "indent=2" to see prettier output.
+        # This doesn't change the ability of bandcamp to use the file.
         json.dump(data, json_file, separators=(',', ':'), ensure_ascii=False)
-        # Add "indent=2" to see prettier output in file
 
 
 if __name__ == "__main__":
